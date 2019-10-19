9a71b4630df0347ee700339b69daf77345158724
Guilherme Deschamps <guilhermerd6@hotmail.com>
2019-10-19 19:21:23 -0300

    Novo front-end em react adicionado ao git

--- backend/src/main/java/Backend/DeliveryMercadoApplication.java
+++ backend/src/main/java/Backend/DeliveryMercadoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DeliveryMercadoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DeliveryMercadoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryMercadoApplication.class, args);
+    }
 
 }
